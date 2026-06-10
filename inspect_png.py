3da3d815be44003c09d05e267f41e71f39a3fd94
Sonam Jaiswal <sonamjaiswal5892@gmail.com>
2026-06-10 19:18:55 +0530

    Clean up: remove node_modules, add .gitignore

--- inspect_png.py
+++
@@ -1,9 +0,0 @@
-﻿from PIL import Image
-path = r'c:\wamp64\www\qr-bell-pages\landing page.png'
-img = Image.open(path)
-print('size', img.size, 'mode', img.mode)
-points=[(50,50),(400,90),(1000,120),(2600,180),(1200,350),(1800,140),(100,900),(100,1200),(1500,2300)]
-for p in points:
-    x,y=p
-    if x<img.width and y<img.height:
-        print(p, img.getpixel(p))
